06f51b1f0418d9f34e96b15f5a050d1b20711495
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2022-08-15 10:47:52 +0200

    🩹Fixed typos in Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,91 +1,93 @@
 import React from "react";
-import Button from "../Button";
+import Link from "../Link";
 
 function Footer() {
   return (
-    <div class="w-full p-8 bg-gray-100 shadow-inner dark:bg-darkest-dark dark:text-white">
-      <div class="mb-4 sm:flex">
-        <div class="h-auto text-3xl text-center sm:w-1/12 sm:text-sm md:text-base lg:text-lg xl:text-2xl">
-          <div class="mb-2 font-bold dark:text-white text-clean-dark-500">
-            Bergflix<span class="text-primary-500">.</span>
+    <div className="w-full p-8 bg-gray-100 shadow-inner dark:bg-darkest-dark dark:text-white">
+      <div className="mb-4 sm:flex">
+        <div className="h-auto text-3xl text-center sm:w-1/12 sm:text-sm md:text-base lg:text-lg xl:text-2xl">
+          <div className="mb-2 font-bold dark:text-white text-clean-dark-500">
+            Bergflix<span className="text-primary-500">.</span>
           </div>
         </div>
-        <div class="h-auto mt-8 sm:w-1/4 sm:mt-0">
-          <div class="mb-2 font-bold text-delorean-500">Social Media</div>
-          <ul class="leading-normal list-reset">
+        <div className="h-auto mt-8 sm:w-1/4 sm:mt-0">
+          <div className="mb-2 font-bold text-delorean-500">Social Media</div>
+          <ul className="leading-normal list-reset">
             <li>
-              <Button type="linkout" to="https://dev.bergflix.de/out/patreon">
+              <Link type="linkout" href="https://dev.bergflix.de/out/patreon">
                 Patreon
-              </Button>
+              </Link>
             </li>
             <li>
-              <Button type="linkout" to="https://dev.bergflix.de/out/patreon">
+              <Link type="linkout" href="https://dev.bergflix.de/out/patreon">
                 Discord
-              </Button>
+              </Link>
             </li>
             <li>
-              <Button type="linkout" to="https://dev.bergflix.de/out/patreon">
+              <Link type="linkout" href="https://dev.bergflix.de/out/patreon">
                 Twitter
-              </Button>
+              </Link>
             </li>
           </ul>
         </div>
-        <div class="h-auto mt-8 sm:w-1/4 sm:mt-0">
-          <div class="mb-2 font-bold text-delorean-500">Bergflix. Brand</div>
-          <ul class="leading-normal list-reset">
+        <div className="h-auto mt-8 sm:w-1/4 sm:mt-0">
+          <div className="mb-2 font-bold text-delorean-500">
+            Bergflix. Brand
+          </div>
+          <ul className="leading-normal list-reset">
             <li>
-              <Button type="link" to="https://dev.bergflix.de/branding">
+              <Link type="link" href="https://dev.bergflix.de/branding">
                 Bergflix. Branding
-              </Button>
+              </Link>
             </li>
             <li>
-              <Button type="link" to="https://dev.bergflix.de/apply">
+              <Link type="link" href="https://dev.bergflix.de/apply">
                 Mithelfen
-              </Button>
+              </Link>
             </li>
             <li>
-              <Button type="linkout" to="https://dev.bergflix.de/out/github">
+              <Link type="linkout" href="https://dev.bergflix.de/out/github">
                 Bergflix Open Source
-              </Button>
+              </Link>
             </li>
           </ul>
         </div>
-        <div class="h-auto mt-8 sm:w-1/4 sm:mt-0">
-          <div class="mb-2 font-bold text-delorean-500">
+        <div className="h-auto mt-8 sm:w-1/4 sm:mt-0">
+          <div className="mb-2 font-bold text-delorean-500">
             Rechtliches & Hilfe
           </div>
-          <ul class="leading-normal list-reset">
+          <ul className="leading-normal list-reset">
             <li>
-              <Button type="link" to="https://dev.bergflix.de/legal/privacy">
-                Datenschutz<span class="hidden md:inline">erklärung</span>
-              </Button>
+              <Link type="link" href="https://dev.bergflix.de/legal/privacy">
+                Datenschutz<span className="hidden md:inline">erklärung</span>
+              </Link>
             </li>
             <li>
-              <Button type="link" to="https://dev.bergflix.de/legal/terms">
+              <Link type="link" href="https://dev.bergflix.de/legal/terms">
                 Cookies & Sicherheit
-              </Button>
+              </Link>
             </li>
             <li>
-              <Button type="link" to="https://dev.bergflix.de/legal/impressum">
+              <Link type="link" href="https://dev.bergflix.de/legal/impressum">
                 Impressum
-              </Button>
+              </Link>
             </li>
           </ul>
         </div>
-        <div class="h-auto mt-8 sm:w-1/4 sm:mt-0">
-          <div class="mb-2 font-bold text-delorean-500">
+        <div className="h-auto mt-8 sm:w-1/4 sm:mt-0">
+          <div className="mb-2 font-bold text-delorean-500">
             Mehr von "Herr Bergmann"
           </div>
-          <ul class="leading-normal list-reset">
+          <ul className="leading-normal list-reset">
             <li>
-              <Button type="linkout" to="https://discord.gg/fromthefuture">
+              <Link type="linkout" href="https://discord.gg/fromthefuture">
                 Joine der “FTF”-Redaktion
-              </Button>
+              </Link>
             </li>
             <li>
-              <Button type="linkout" to="https://www.patreon.com/herrbergmann">
+              <Link type="linkout" href="https://www.patreon.com/herrbergmann">
                 Unterstütze seine Projekte
-              </Button>
+              </Link>
             </li>
           </ul>
         </div>
