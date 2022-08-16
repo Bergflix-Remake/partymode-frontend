ce30887f42d17dc93c0d210867e64d83f3a334ad
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2022-08-16 14:51:52 +0200

    🐛Resolved ESLint errors

--- src/components/Input/TextField/index.js
+++ src/components/Input/TextField/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function TextField({ type, style, placeholder, ...props }) {
+function TextField({ type, _style, placeholder, ...props }) {
   return (
     <input
-      className={`h-min p-2 border-2 border-primary-500 rounded bg-transparent outline-none ${style}`}
+      className={`h-min p-2 border-2 border-primary-500 rounded bg-transparent outline-none ${_style}`}
       type={type}
       placeholder={placeholder}
       {...props}
